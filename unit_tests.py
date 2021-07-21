ef6383753149296767b9db38250461f232a51d6e
alvinli04 <alvin.li.j@gmail.com>
2021-07-21 14:25:34 -0400

    finished neqr test

--- unit_tests.py
+++ unit_tests.py
@@ -1,15 +1,36 @@
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from qiskit import execute
+from qiskit import Aer
+from qiskit import IBMQ
+from qiskit.compiler import transpile
+
 import neqr
-import qiskit
 import random
 
-def test1(): 
+def convert_to_bits_test(): 
     array2x2 = [[random.randint(0, 255), random.randint(0, 255)], [random.randint(0, 255), random.randint(0, 255)]]
     print(array2x2)
     bits_arr = neqr.convert_to_bits(array2x2)
     print(bits_arr)
 
+def neqr_test():
+    array2x2 = [[random.randint(0, 255), random.randint(0, 255)], [random.randint(0, 255), random.randint(0, 255)]]
+    print('2x2 array:')
+    print(array2x2)
+
+    flattened_array = neqr.convert_to_bits(array2x2)
+    result_circuit = neqr.neqr(flattened_array)
+
+    backend = Aer.get_backend('statevector_simulator')
+    job = execute(result_circuit, backend=backend, shots=1, memory=True)
+    job_result = job.result()
+    statevec = job_result.get_statevector(result_circuit)
+    for i in range(len(statevec)):
+        if statevec[i] != 0:
+            print(f"{format(i, '02b')}: {statevec[i].real}")
+
 def main():
-    test1()
+    convert_to_bits_test()
 
 if __name__ == '__main__':
     main()
