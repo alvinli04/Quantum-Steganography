59ae61723fa67f34e44f11f71a69646e1ce9394e
dssikdar <dssikdar@gmail.com>
2021-07-22 17:53:28 -0400

    Think it works. Just need to unit test.

--- comparator.py
+++ comparator.py
@@ -1,51 +1,26 @@
 import qiskit
-
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 from qiskit import execute
 from qiskit import Aer
 from qiskit import IBMQ
 from qiskit.circuit.quantumregister import AncillaRegister
-from qiskit.compiler import transpile
-from time import perf_counter
-from qiskit.tools.visualization import plot_histogram
 
-import numpy as np
-
-def compare (register, c):
-    if register[0] > register[1]: 
-        c[0] = 1
-        c[1] = 0
-    elif register[0] < register[1]: 
-        c[0] = 0
-        c[1] = 1
-    elif register[0] == register[1]:
-        c[0] = 0
-        c[1] = 0
-        print("continuing")
-    return c
-    
 def comparator(regX, regY): 
     qc = QuantumCircuit(regX, regY)
     # regX and regY should have the same size 
     regLength = regX.size 
-    ancilla = AncillaRegister(2)
+    ancilla = AncillaRegister(2*regLength)
     qc.add_register(ancilla)
-
-    for index in range(regLength): 
-        qc.x(regY[index])
-        #qc.ccx(regY[index], regX[index], ancilla[0])
-        print("anc: ", ancilla, "\n")
-        qc.x(regY[index])
+    for index in range(regLength):
         qc.x(regX[index])
-        #qc.ccx(regY[index], regX[index], ancilla[1])
-    return ancilla
-
+        qc.mcx([regY[index], regX[index]]+[ancilla[i] for i in range(2*index)], [ancilla[index*2]])
+        qc.x(regX[index])
+        qc.x(regY[index])
+        qc.mcx([regY[index], regX[index]]+[ancilla[i] for i in range(2*index)], [ancilla[index*2+1]])
+    return qc
 if __name__ == '__main__':
-    regX = QuantumRegister(2)
-    regY = QuantumRegister(2)
+    regX = QuantumRegister(2, "x")
+    regY = QuantumRegister(2, "y")
     cResult = comparator(regX, regY)
     print("Comparator Register Output")
-    #print("c0: ", cResult[0], "c1: ", cResult[1], "\n")
-
-        
-
+    print(comparator(regX, regY))
