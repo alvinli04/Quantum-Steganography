e5983ee24a4b18b02f9c0acd793e29af5f81a5aa
dssikdar <dssikdar@gmail.com>
2021-07-23 03:01:23 -0400

    comparator ready for testing

--- comparator_old.py
+++ comparator_old.py
@@ -11,25 +11,17 @@ def comparator(regX, regY):
     regLength = regX.size 
     ancilla = AncillaRegister(2*regLength)
     qc.add_register(ancilla)
-    qc.x(ancilla)
     for index in range(regLength):
         qc.x(regX[index])
         qc.mcx([regY[index], regX[index]]+[ancilla[i] for i in range(2*index)], [ancilla[index*2]])
-        if index < regLength-1: 
-            qc.mcx([regY[index], regX[index]]+[ancilla[i] for i in range(2*index)], [ancilla[2*regLength-2]])
         qc.x(regX[index])
         qc.x(regY[index])
         qc.mcx([regY[index], regX[index]]+[ancilla[i] for i in range(2*index)], [ancilla[index*2+1]])
-        if index < regLength-1: 
-            qc.mcx([regY[index], regX[index]]+[ancilla[i] for i in range(2*index)], [ancilla[2*regLength-1]])
-        qc.x(regY[index])
-        
-    qc.x(ancilla)
     return qc
 
 if __name__ == '__main__':
-    regX = QuantumRegister(2, "x")
-    regY = QuantumRegister(2, "y")
+    regX = QuantumRegister(4, "x")
+    regY = QuantumRegister(4, "y")
     cResult = comparator(regX, regY)
     print("Comparator Register Output")
     print(comparator(regX, regY))
