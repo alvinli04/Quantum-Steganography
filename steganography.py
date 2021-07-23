51401d05d55b88de784c1df61d60c371d36ccd36
dssikdar <dssikdar@gmail.com>
2021-07-23 11:01:40 -0400

    Comparator and its unit test is working

---
+++ steganography.py
@@ -0,0 +1,174 @@
+import qiskit
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister, AncillaRegister
+from qiskit import execute
+from qiskit import Aer
+from qiskit import IBMQ
+from qiskit.compiler import transpile
+from time import perf_counter
+from qiskit.tools.visualization import plot_histogram
+from qiskit.circuit.library import SXdgGate
+
+import numpy as np
+import neqr
+import random
+import math
+
+'''
+params
+---------------
+Y: a quantum register
+X: a quantum register
+return
+---------------
+a size 2 register, c0 c1:
+If c1c0 = 00, then Y = X.
+If c1c0 = 01, then Y < X.
+If c1c0 = 10, then Y > X
+'''
+def comparator(regY, regX, circuit, result): 
+    # regX and regY should have the same size 
+    regLength = regX.size 
+    ancilla = AncillaRegister(2*regLength)
+    circuit.add_register(ancilla)
+
+    circuit.x(ancilla)
+    for index in range(regLength):
+        circuit.x(regX[index])
+        circuit.mcx([regY[index], regX[index]]+[ancilla[i] for i in range(2*index)], [ancilla[index*2]])
+        if index < regLength-1: 
+            circuit.mcx([regY[index], regX[index]]+[ancilla[i] for i in range(2*index)], [ancilla[2*regLength-2]])
+        circuit.x(regX[index])
+        circuit.x(regY[index])
+        circuit.mcx([regY[index], regX[index]]+[ancilla[i] for i in range(2*index)], [ancilla[index*2+1]])
+        if index < regLength-1: 
+            circuit.mcx([regY[index], regX[index]]+[ancilla[i] for i in range(2*index)], [ancilla[2*regLength-1]])
+        circuit.x(regY[index])     
+    circuit.x(ancilla)
+
+    circuit.cx(ancilla[2*regLength-2], result[0])
+    circuit.cx(ancilla[2*regLength-1], result[1])
+
+    return circuit
+
+
+'''
+params
+---------------
+YX: a quantum register containing two coordinates, |Y>|X>
+AB: a quantum register containing two coordinates, |A>|B>
+return
+---------------
+A single qubit |r> which is |1> when YX = AB and |0> otherwise
+'''
+def coordinate_comparator(circuit, YX, AB):
+    n = YX.size
+    result = QuantumRegister(1, 'result')
+    circuit.add_register(result)
+    
+    for i in range(n):
+        circuit.x(YX[i])
+        circuit.cx(YX[i], AB[i])
+        circuit.x(YX[i])
+        
+    circuit.mcx(AB, result)
+    
+    for i in range(n):
+        circuit.x(YX[i])
+        circuit.cx(YX[i], AB[i])
+        circuit.x(YX[i])
+    
+    return circuit
+
+
+'''
+params
+---------------
+Y: a quantum register
+X: a quantum register
+return
+---------------
+A quantum register |D> which holds the positive difference of Y and X.
+'''
+def difference(circuit, Y, X):
+    # PART 1: 
+    # reversible parallel subtractor
+    
+    # initialize registers to store sign, difference, and junk qubits
+    regLength = X.size
+    sign = QuantumRegister(1, 'sign')
+    difference = QuantumRegister(regLength, 'difference')
+    ancilla = QuantumRegister(regLength - 1, 'junk')
+    circuit.add_register(ancilla, difference)
+
+    # perform half subtractor for first qubit
+    rev_half_subtractor(circuit, X[0], Y[0], sign[0], ancilla[0])
+    
+    # perform full subtrator for rest of qubits
+    for i in range(1, regLength - 1): 
+        rev_full_subtractor(circuit, X[i], Y[i], ancilla[i-1], difference[i-1], ancilla[i])
+    rev_full_subtractor(circuit, X[-1], Y[-1], ancilla[-1], difference[-2], difference[-1])
+    
+    # swap X and difference registers to fix result 
+    # this is just sort of a thing you have to do
+    circuit.swap(sign, X[0])
+    for i in range(1, regLength): 
+        circuit.swap(difference[i-1], X[i])
+    
+    # PART 2: 
+    # complementary operation
+
+    # flip the difference based on the sign (pt 1)
+    for i in range(regLength): 
+        circuit.cx(sign[0], difference[i])
+    
+    # flip the difference again, but based on sign and remaining bits (pt 2)
+    for i in range(regLength):
+        circuit.mcx([sign[0] + difference[i+1:]], difference[i])
+    
+    return circuit
+
+
+
+'''
+params
+---------------
+regA: a quantum register, one of the numbers being subtracted
+regB: a quantum register, one of the numbers being subtracted
+Q: Updated depending on result
+Borrow: Digit to be carried over
+return
+---------------
+Performs A - B, and updates results into Q and Borrow 
+'''
+def rev_half_subtractor(circuit, A, B, Q, Borrow): 
+    csxdg_gate = SXdgGate().control()
+    circuit.append(csxdg_gate, [A, Borrow])
+    circuit.cx(A, Q)
+    circuit.cx(B, A)
+    circuit.csx(B, Borrow)
+    circuit.csx(A, Borrow)
+    circuit.barrier()
+    
+    
+'''
+params
+---------------
+regA: a quantum register, one of the numbers being subtracted
+regB: a quantum register, one of the numbers subtracting
+regC: a quantum register, one of the numbers subtracting
+Q: Updated depending on result
+Borrow: Digit to be carried over
+return
+---------------
+Performs A - B - C, updates results into Q and Borrow
+'''
+def rev_full_subtractor(circuit, A, B, C, Q, Borrow): 
+    csxdg_gate = SXdgGate().control()
+    circuit.append(csxdg_gate, [A, Borrow])
+    circuit.cx(A, Q)
+    circuit.cx(B, A)
+    circuit.csx(B, Borrow)
+    circuit.cx(C, A)
+    circuit.csx(C, Borrow)
+    circuit.csx(A, Borrow)
+    circuit.barrier()
--- unit_tests.py
+++ unit_tests.py
@@ -6,7 +6,15 @@ from qiskit.compiler import transpile
 
 import neqr
 import random
+import steganography
 
+
+def arraynxn(n):
+    return [[random.randint(0,255) for i in range(n)] for j in range(n)]
+
+'''
+NEQR Unit Tests
+'''
 def convert_to_bits_test(): 
     array2x2 = [[random.randint(0, 255), random.randint(0, 255)], [random.randint(0, 255), random.randint(0, 255)]]
     print(array2x2)
@@ -14,12 +22,14 @@ def convert_to_bits_test():
     print(bits_arr)
 
 def neqr_test():
-    array2x2 = [[random.randint(0, 255), random.randint(0, 255)], [random.randint(0, 255), random.randint(0, 255)]]
-    print('2x2 array:')
-    print(array2x2)
-    print(neqr.convert_to_bits(array2x2))
+    testarr = arraynxn(4)
+    print('test array:')
+    print(testarr)
 
-    flattened_array = neqr.convert_to_bits(array2x2)
+    flattened_array = neqr.convert_to_bits(testarr)
+
+    print([''.join([str(b) for i,b in enumerate(a)]) for a in flattened_array])
+
     result_circuit = neqr.neqr(flattened_array)
 
     backend = Aer.get_backend('statevector_simulator')
@@ -28,11 +38,99 @@ def neqr_test():
     statevec = job_result.get_statevector(result_circuit)
     for i in range(len(statevec)):
         if statevec[i] != 0:
-            print(f"{format(i, '010b')}: {statevec[i].real}")
+            print(f"{format(i, '012b')}: {statevec[i].real}")
+    print(result_circuit)
+
+############################################################################################################################
+
+'''
+Steganography Unit Tests
+'''
+def comparator_test():
+    #creating registers and adding them to circuit 
+    regX = QuantumRegister(4)
+    regY = QuantumRegister(4)
+    circuit = QuantumCircuit(regX, regY)
+    cr = ClassicalRegister(2)
+
+    #changing registers to make them different 
+    circuit.x(regX[0])
+    circuit.x(regX[2])
+    circuit.x(regY[1])
+    circuit.x(regY[3])
+
+    result = QuantumRegister(2, 'result')
+    circuit.add_register(result)
+
+    circuit.add_register(cr)
+    
+    #comparator returns the circuit
+    resultCircuit = steganography.comparator(regY, regX, circuit, result)
+    #result --> ancillas from function
+    resultCircuit.measure(result, cr)
+
+    #measuring
+    simulator = Aer.get_backend('aer_simulator')
+    simulation = execute(resultCircuit, simulator, shots=1, memory=True)
+    simResult = simulation.result()
+    counts = simResult.get_counts(resultCircuit)
+    for(state, count) in counts.items():
+        big_endian_state = state[::-1]
+        return big_endian_state   
+
+
+def coordinate_comparator_test():
+    regXY = QuantumRegister(2, "reg1")
+    regAB = QuantumRegister(2, "reg2")
+    circuit = QuantumCircuit(regXY, regAB)
+    #uncomment this to make the registers different
+    #regXY is in |00> and regAB in |01>
+    #circuit.x(regAB[1])
+    #circuit.x(regXY[1])
+    resultCircuit = steganography.coordinate_comparator(circuit, regXY, regAB)
+    print(resultCircuit.draw())
+    backend = Aer.get_backend('statevector_simulator')
+    simulation = execute(resultCircuit, backend=backend, shots=1, memory=True)
+    simResult = simulation.result()
+    statevec = simResult.get_statevector(resultCircuit)
+    for state in range(len(statevec)):
+        if statevec[state] != 0:
+            #note: output is in little endian
+            print(f"{format(state, '05b')}: {statevec[state].real}")
+
+def difference_test():
+    regY = QuantumRegister(4, "regY")
+    regX = QuantumRegister(4, "regX")
+    circuit = QuantumCircuit(regY, regX)
+
+    #circuit.x(regY)
+    #circuit.x(regX)
+    circuit.barrier()
+
+    resultCircuit = steganography.difference(circuit, regY, regX)
+    print(resultCircuit.draw())
+    backend = Aer.get_backend('statevector_simulator')
+    simulation = execute(resultCircuit, backend=backend, shots=1, memory=True)
+    simResult = simulation.result()
+    statevec = simResult.get_statevector(resultCircuit)
+    for state in range(len(statevec)):
+        if statevec[state] != 0:
+            #note: output is in little endian
+            #only have to look at first bit 
+            print(f"{format(state, '05b')}: {statevec[state].real}")
+
+
+def get_secret_image_test():
+    test_arr = [[[random.randint(0,1) for i in range(4)] for j in range(4)] for k in range(5)]
+    test_result = steganography.get_secret_image(5, test_arr)
+    for a in test_arr:
+        print(a)
+    print(f'result:\n {test_result}')
 
 def main():
-    convert_to_bits_test()
-    neqr_test()
+    #get_secret_image_test()
+    print("\n Comparator Test: ", comparator_test(), "\n")
+    coordinate_comparator_test()
 
 if __name__ == '__main__':
     main()
