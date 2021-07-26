e712f123201bfa314de930c49d2893f9112bf0f3
Jessica Yin <jessicayin08@gmail.com>
2021-07-26 10:23:54 -0400

    Added controlled difference and corresponding functions, and pt 1 & 4 of embedding code

--- steganography.py
+++ steganography.py
@@ -1,5 +1,5 @@
 import qiskit
-from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
+from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister, AncillaRegister
 from qiskit import execute
 from qiskit import Aer
 from qiskit import IBMQ
@@ -7,7 +7,6 @@ from qiskit.compiler import transpile
 from time import perf_counter
 from qiskit.tools.visualization import plot_histogram
 from qiskit.circuit.library import SXdgGate
-from qiskit.circuit.quantumregister import AncillaRegister
 
 
 import numpy as np
@@ -83,7 +82,7 @@ def coordinate_comparator(circuit, YX, AB):
         circuit.cx(YX[i], AB[i])
         circuit.x(YX[i])
     
-    return circuit
+    return (circuit, result)
 
 
 '''
@@ -183,6 +182,82 @@ def rev_full_subtractor(circuit, A, B, C, Q, Borrow):
     circuit.barrier()
 
 
+def controlled_difference(controlled_qubit, circuit, Y, X, difference): 
+    # PART 1: 
+    # reversible parallel subtractor
+    
+    # initialize registers to store sign, difference, and junk qubits
+    regLength = X.size
+    sign = QuantumRegister(1, 'sign')
+    ancilla = QuantumRegister(regLength - 1, 'junk')
+    circuit.add_register(ancilla)
+    circuit.add_register(sign)
+
+    # perform half subtractor for last qubit
+    controlled_rhs(controlled_qubit, circuit, X[-1], Y[-1], difference[-1], ancilla[-1])
+    
+    # perform full subtrator for rest of qubits
+    for i in range(regLength - 2, 0, -1): 
+        controlled_rfs(controlled_qubit, circuit, X[i], Y[i], ancilla[i], difference[i], ancilla[i-1])
+    controlled_rfs(controlled_qubit, circuit, X[0], Y[0], ancilla[0], difference[0], sign[0])
+    
+    # swap X and difference registers to fix result 
+    # this is just sort of a thing you have to do
+    for i in range(regLength - 1, -1, -1): 
+        circuit.cswap(controlled_qubit, difference[i], X[i])
+    
+    # PART 2: 
+    # complementary operation
+
+    # flip the difference based on the sign (pt 1)
+    for i in range(regLength): 
+        circuit.ccx(controlled_qubit, sign[0], difference[i])
+    
+    # flip the difference again, but based on sign and remaining bits (pt 2)
+    #for i in range(regLength-1, -1, -1):
+    for i in range(regLength):
+        circuit.mcx([controlled_qubit] + [sign[0]] + difference[i+1:], difference[i])
+    
+
+# controlled reversible half subtractor
+def controlled_rhs(controlled_qubit, circuit, A, B, Q, Borrow): 
+    # allocate ancilla qubits
+    anc1 = QuantumRegister(3)
+    circuit.add_register(anc1)
+
+    # rewritten code to control for given qubit
+    csxdg_gate = SXdgGate().control()
+    circuit.ccx(controlled_qubit, A, anc1[0])
+    circuit.append(csxdg_gate, [anc1[0], Borrow])
+    circuit.ccx(controlled_qubit, A, Q)
+    circuit.ccx(controlled_qubit, B, A)
+    circuit.ccx(controlled_qubit, B, anc1[1])
+    circuit.ccx(controlled_qubit, A, anc1[2])
+    circuit.csx(anc1[1], Borrow)
+    circuit.csx(anc1[2], Borrow)
+    circuit.barrier()
+
+# controlled reversible full subtractor
+def controlled_rfs(controlled_qubit, circuit, A, B, C, Q, Borrow): 
+    # allocate ancilla qubits
+    anc2 = QuantumRegister(4)
+    circuit.add_register(anc2)
+    
+    # rewritten code to control for given qubit
+    csxdg_gate = SXdgGate().control()
+    circuit.ccx(controlled_qubit, A, anc2[0])
+    circuit.append(csxdg_gate, [anc2[0], Borrow])
+    circuit.cx(A, Q)
+    circuit.cx(B, A)
+    circuit.ccx(controlled_qubit, B, anc2[1])
+    circuit.csx(anc2[1], Borrow)
+    circuit.cx(C, A)
+    circuit.ccx(controlled_qubit, C, anc2[2])
+    circuit.csx(anc2[2], Borrow)
+    circuit.ccx(controlled_qubit, A, anc2[3])
+    circuit.csx(anc2[3], Borrow)
+    circuit.barrier()
+
 '''
 Embedding Procedure
 '''
@@ -235,4 +310,42 @@ key: the quantum key
 '''
 def get_key(cover_image, secret_image, image_size):
     key = QuantumCircuit(image_size)
+
+
+def embed(circuit, C, S, Key):
+    ### PART ONE
+    
+    _, r_1 = coordinate_comparator(circuit, C, S)
+    _, r_2 = coordinate_comparator(circuit, C, Key)
+
+    ### PART FOUR
+    # placeholder code to declare variables so 
+    # vs code won't get all uppity
+    cover_image_values = QuantumRegister(1) # not real size
+    secret_image_values = QuantumRegister(1) # not real size
+    comparator_result = QuantumRegister(2)
+    circuit.add_register(cover_image_values)
+    circuit.add_register(secret_image_values)
+    circuit.add_register(comparator_result)
+
+    # this is part of the actual code now
+
+    key_i = QuantumRegister(1, 'key_i')
+    circuit.add_register(key_i)
+
+    # flip for zero-controlled ccnots
+    circuit.x(comparator_result[1])
+    circuit.ccx(r_1, comparator_result[1], cover_image_values)
+    circuit.ccx(r_1, comparator_result[1], secret_image_values)
+    circuit.x(comparator_result[1]) # flip back
+
+    # do a cute little toffoli cascade for the cccswap and cccx
+    anc = QuantumRegister(2) # allocate ancilla qubits
+    circuit.add_register(anc)
+    circuit.ccx(r_1, r_2, anc[0])
+    circuit.ccx(anc[0], comparator_result[1], anc[1])
+    circuit.cswap(anc[2], cover_image_values, secret_image_values) # cccswap
+    circuit.cx(comparator_result[1], key_i[0]) # cccnot
+
+
 
